12b388d01597e2ede0a8346a09b4e449a8eff9f9
wyericso <eric@woooa.net>
2018-10-22 17:41:16 +0800

    build static html look

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 
 const express = require('express');
 const app = express();
-
+/*
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
+*/
+
+app.use(express.static('views'));
 
 const listener = app.listen(80, function() {
     console.log('Listening on port ' + listener.address().port);
